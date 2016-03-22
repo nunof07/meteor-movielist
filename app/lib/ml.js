467ac0943ae88e6a3f96fadb1bdf0031ce8ac261
Nuno Freitas <nunofreitas@gmail.com>
2016-03-22 13:51:25 +0000

    Refractor lists/collections

---
+++ app/lib/ml.js
@@ -0,0 +1,113 @@
+ML = {};
+ML.createCollection = createCollection;
+ML.createMethods = createMethods;
+
+function createCollection(name, fields, publicFields) {
+    const collection = new Mongo.Collection(name);
+    
+    denyClientSideUpdates(collection);
+    createSchema(collection, fields);
+    createPublicFields(collection, publicFields);
+    collection.methods = {};
+    
+    return collection;
+    
+    function denyClientSideUpdates(collection) {
+        collection.deny({
+            insert: denyAll,
+            update: denyAll,
+            remove: denyAll,
+        });
+        return collection;
+        
+        function denyAll() {
+            return true;
+        }
+    }
+    function createSchema(collection, fields) {
+        collection.fields = fields;
+        collection.schema = new SimpleSchema(collection.fields);
+        collection.attachSchema(collection.schema);
+        
+        return collection.schema;
+    }
+    function createPublicFields(collection, publicFields) {
+        collection.publicFields = {};
+        
+        for (let i = 0; i < publicFields.length; i++) {
+            const field = publicFields[i];
+            collection.publicFields[field] = 1;
+        }
+        
+        return collection.publicFields;
+    }
+}
+function createMethods(collection, declarations) {
+    for (let i = 0; i < declarations.length; i++) {
+        createMethod(collection,
+            declarations[i].name,
+            declarations[i].fields,
+            declarations[i].run);
+    }
+    rateLimitMethods(collection);
+    
+    return collection;
+}
+function createMethod(collection, name, fieldNames, callback) {
+    collection.methods[name] = new ValidatedMethod({
+        name: collection._name + '.' + name,
+        validate: getValidator(collection, fieldNames),
+        run: methodBody
+    });
+    return collection.methods[name];
+    
+    function methodBody() {
+        return callback.apply(this, arguments);
+    }
+    function getValidator(collection, fieldNames) {
+        const fields = getValidatorFields(collection, fieldNames);
+        return new SimpleSchema(fields).validator();
+        
+        function getValidatorFields(collection, fieldNames) {
+            const result = {};
+            
+            for (let i = 0; i < fieldNames.length; i++) {
+                const fieldName = fieldNames[i];
+                const field = collection.fields[fieldName];
+                result[fieldName] = field;
+            }
+            
+            return result;
+        }
+    }
+}
+function rateLimitMethods(collection, numRequests, timeInterval) {
+    numRequests = numRequests || 5;
+    timeInterval = timeInterval || 1000;
+    const methodNames = getMethodNames(collection);
+    
+    DDPRateLimiter.addRule({
+        name: limitByName,
+        connectionId: limitByConnectionId
+    }, numRequests, timeInterval);
+    return;
+    
+    function getMethodNames(collection) {
+        const methodNames = [];
+    
+        for (const method in collection.methods) {
+            if (collection.methods.hasOwnProperty(method)) {
+                const name = collection.methods[method].name;
+                methodNames.push(name);
+            }
+        }
+        
+        return methodNames;
+    }
+    function limitByName(name) {
+        return _.contains(methodNames, name);
+    }
+    function limitByConnectionId() {
+        return true;
+    }
+}
--- app/lists/lists.js
+++ app/lists/lists.js
@@ -1,77 +1,40 @@
-Lists = new Mongo.Collection('Lists');
-
-// deny all client-side updates
-Lists.deny({
-    insert() { return true; },
-    update() { return true; },
-    remove() { return true; },
-});
-
-// schema
-Lists.fields = {
+Lists = ML.createCollection('Lists', {
     name: { type: String },
     isPublic: { type: Boolean },
     ownerId: { type: String, regEx: SimpleSchema.RegEx.Id, denyUpdate: true },
     createdAt: { type: Date, denyUpdate: true },
     modifiedAt: { type: Date },
-};
-Lists.schema = new SimpleSchema(Lists.fields);
-Lists.attachSchema(Lists.schema);
+}, ['name', 'isPublic', 'ownerId']);
 
-// public fields
-Lists.publicFields = {
-    name: 1,
-    isPublic: 1,
-    ownerId: 1
-};
-
-// helpers
 Lists.helpers({
-    isEditableBy() {
-        if (Meteor.userId()) {
-            return this.ownerId === Meteor.userId();
-        }
-        
-        return false;
-    }
+    isEditableBy: listIsEditableBy
 });
 
-// methods
-Lists.methods = {};
-Lists.methods.insert = new ValidatedMethod({
-    name: 'Lists.methods.insert',
-    validate: new SimpleSchema({
-        name: Lists.fields.name,
-        isPublic: Lists.fields.isPublic
-    }).validator(),
-    run({ name, isPublic }) {
-        if (!this.userId) {
-            throw new Meteor.Error('Lists.methods.insert.unauthorized',
-                'Must be logged in to create a new list');
-        }
-        const list = {
-            name,
-            isPublic,
-            ownerId: this.userId,
-            createdAt: new Date(),
-            modifiedAt: new Date(),
-        };
-        
-        return Lists.insert(list);
+ML.createMethods(Lists, [
+    {
+        name: 'insert',
+        fields: ['name', 'isPublic'],
+        run: listsInsert
     }
-});
+]);
 
-// list of all methods
-const LISTS_METHODS = _.pluck([
-    Lists.methods.insert
-], 'name');
-
-// only allow 5 operations per connection per second
-DDPRateLimiter.addRule({
-    name(name) {
-        return _.contains(LISTS_METHODS, name);
-    },
-
-    // rate limit per connection ID
-    connectionId() { return true; }
-}, 5, 1000);
+function listIsEditableBy() {
+    if (Meteor.userId()) {
+        return this.ownerId === Meteor.userId();
+    }
+    
+    return false;
+}
+function listsInsert({name, isPublic}) {
+    if (!this.userId) {
+        throw new Meteor.Error('unauthorized', 'Must be logged in to create a new list');
+    }
+    
+    return Lists.insert({
+        name,
+        isPublic,
+        ownerId: this.userId,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+    });
+}
