24135e37ec0bfed70bd718752a039edbe788ca29
Nuno Freitas <nunofreitas@gmail.com>
2016-04-02 12:14:36 +0100

    Fixed issue when settings not available

--- app/movies/server/moviedb.js
+++ app/movies/server/moviedb.js
@@ -1,1 +1,3 @@
-MovieDb = Meteor.npmRequire('moviedb')(Meteor.settings.private.tmdb);
+if (Meteor.settings.private && Meteor.settings.private.tmdb) {
+    MovieDb = Meteor.npmRequire('moviedb')(Meteor.settings.private.tmdb);
+}
