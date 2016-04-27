3ca1ed4432c576beb89d3b1593a56a218e858f16
Nuno Freitas <nunofreitas@gmail.com>
2016-04-27 23:25:26 +0100

    Updated interface for vote control and star scores

--- app/scores/client/vote/vote.component.js
+++ app/scores/client/vote/vote.component.js
@@ -18,14 +18,49 @@ function voteDirective() {
 function VoteController($scope, $reactive, logger, errorService) {
     const ctrl = this;
     $reactive(ctrl).attach($scope);
+    ctrl.subscribe('movieScore.user', getMovieId);
     
     ctrl.error = false;
     ctrl.errorMessage = false;
+    ctrl.score = 0;
+    ctrl.starScore = 0;
     
     ctrl.vote = vote;
     ctrl.dismissError = dismissError;
+    
+    ctrl.autorun(scoreAutorun);
     return;
     
+    function getMovieId() {
+        const movie = ctrl.getReactively('movie');
+        
+        if (movie) {
+            return [{ movieId: movie._id }];
+        } else {
+            return [{ }];
+        }
+    }
+    function scoreAutorun() {
+        const movie = ctrl.getReactively('movie');
+        let updated = false;
+        
+        if (movie && Meteor.userId()) {
+            const score = Scores.findOne({
+                userId: Meteor.userId(),
+                movieId: ctrl.movie._id});
+                
+            if (score) {
+                ctrl.score = score.score;
+                ctrl.starScore = 2 * ctrl.score;
+                updated = true;
+            }
+        }
+        
+        if (!updated) {
+            ctrl.score = 0;
+            ctrl.starScore = 0;
+        }
+    }
     function vote(score) {
         if (ctrl.movie && score >= 1 && score <= 5) {
             ctrl.error = false;
--- app/scores/server/scores.publications.js
+++ app/scores/server/scores.publications.js
@@ -1,4 +1,6 @@
 Meteor.publish('movieScore.all', publishMovieScore);
+Meteor.publish('movieScore.user', publishUserMovieScore);
+Meteor.publish('movieScores.user', publishUserMovieScores);
 
 function publishMovieScore({ movieId }) {
     validate();
@@ -24,6 +26,43 @@ function publishMovieScore({ movieId }) {
             Counts.publish(this, 'MovieScore.total',
                 Scores.find(selector, { fields: { score: 1 } }),
                 { countFromField: 'score' });
+                
+            return Scores.find(selector, Scores.publicFields);
+        }
+    }
+}
+function publishUserMovieScore({ movieId }) {
+    validate();
+    this.autorun(autorun);
+    return;
+    
+    function validate() {
+         new SimpleSchema({
+            movieId: ML.fields.id
+        }).validate({ movieId });
+    }
+    function autorun(compution) {
+        if (!this.userId) {
+            return this.ready();
+        } else {
+            const selector = {
+                userId: this.userId,
+                movieId
+            };
+                
+            return Scores.find(selector, Scores.publicFields);
+        }
+    }
+}
+function publishUserMovieScores() {
+    this.autorun(autorun);
+    return;
+    
+    function autorun(compution) {
+        if (!this.userId) {
+            return this.ready();
+        } else {
+            const selector = { userId: this.userId };
                 
             return Scores.find(selector, Scores.publicFields);
         }
