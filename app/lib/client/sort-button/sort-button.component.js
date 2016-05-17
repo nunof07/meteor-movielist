086bf0f9ec87dffe7709b6127fca827708201b46
Nuno Freitas <nunofreitas@gmail.com>
2016-05-17 22:41:39 +0100

    Better icons for sort buttons

--- app/lib/client/sort-button/sort-button.component.js
+++ app/lib/client/sort-button/sort-button.component.js
@@ -10,6 +10,7 @@ function sortButtonDirective() {
             value: '=mlValue',
             expression: '=mlExpression',
             isReverse: '=mlIsReverse',
+            icon: '=mlIcon'
         },
         bindToController: true,
         templateUrl: 'app/lib/client/sort-button/sort-button.html',
@@ -23,10 +24,32 @@ function SortButtonController($scope, $reactive) {
     $reactive(ctrl).attach($scope);
     
     ctrl.sort = sort;
+    
+    ctrl.autorun(iconClass);
     return;
     
     function sort() {
         ctrl.expression = ctrl.value;
         ctrl.isReverse = !ctrl.isReverse;
+    }
+    function iconClass() {
+        const mlIcon = ctrl.getReactively('icon');
+        const isReverse = ctrl.getReactively('isReverse');
+        let icon = 'i-sort';
+        
+        switch (mlIcon) {
+            case 'alpha':
+            case 'amount':
+                icon += '-' + mlIcon;
+                break;
+            default: break;
+        }
+        
+        if (isReverse) {
+            icon += '-desc';
+        } else {
+            icon += '-asc';
+        }
+        ctrl.iconClass = icon;
     }
 }
