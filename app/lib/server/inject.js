bdcde6fbc2390154d555d4b3e7d02ed99661ccf4
Nuno Freitas <nunofreitas@gmail.com>
2016-05-25 23:45:27 +0100

    Added theme + styling fixes

--- app/lib/server/inject.js
+++ app/lib/server/inject.js
@@ -1,3 +1,5 @@
 Inject.rawHead('meta-charset', '<meta charset="utf-8">');
 Inject.rawHead('meta-ieedge', '<meta http-equiv="X-UA-Compatible" content="IE=edge">');
 Inject.rawHead('meta-viewport', '<meta name="viewport" content="width=device-width, initial-scale=1">');
+Inject.rawHead('font-news-cycle',
+    '<link href="https://fonts.googleapis.com/css?family=News+Cycle:400,700" rel="stylesheet" type="text/css">');
