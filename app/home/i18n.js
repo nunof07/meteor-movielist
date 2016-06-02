e71c959129cf58883e5486cfe39dbc1549be58dc
Nuno Freitas <nunofreitas@gmail.com>
2016-06-02 23:43:22 +0100

    Homepage

--- app/home/i18n.js
+++ app/home/i18n.js
@@ -4,7 +4,34 @@ function i18nHomeStartup() {
     i18n.map('en', {
         home: {
             title: 'Home',
-            notLoggedIn: 'Please login to manage your lists.'
+            notLoggedIn: 'Please login to manage your lists.',
+            features: {
+                header: {
+                    title: 'MovieList',
+                    description: 'An open source web app for managing movie lists'
+                },
+                manageLists: {
+                    title: 'Manage lists',
+                    content: '<ul class="list-unstyled">' +
+                        '<li>Create private or public lists</li>' +
+                        '<li>Add movies from <a href="https://www.themoviedb.org/">TMDb</a></li>' +
+                        '</ul>'
+                },
+                multipleViews: {
+                    title: 'Multiple views',
+                    content: '<ul class="list-unstyled">' +
+                        '<li>Switch between list and grid view</li>' +
+                        '<li>Sort and filter movies on different criteria</li>' +
+                        '</ul>'
+                },
+                recomendations: {
+                    title: 'Movie recomendations',
+                    content: '<ul class="list-unstyled">' +
+                        '<li>Vote and see movie votes</li>' +
+                        '<li>Get suggestions for what to watch</li>' +
+                        '</ul>'
+                }
+            }
         }
     });
 }
